96055e9eb16011d451372e835e27b3ee12e533f2
draichi <lucasdraichi@gmail.com>
2020-01-16 23:05:18 -0300

    updates test :bug:

--- test/Token.js
+++ test/Token.js
@@ -12,30 +12,25 @@ contract('Token', accounts => {
     assert.equal(totalSupply.toNumber(), 1000000, 'sets the total supply to 1000000')
     assert.equal(adminBalance.toNumber(), 1000000, 'it allocates the initial supply to the admin')
   })
-  it('transfers token ownership', () => {
-    return Token.deployed().then(instance => {
-      tokenInstance = instance
-      return tokenInstance.transfer.call(accounts[1], 999999999999999)
-    }).then(assert.fail).catch(error => {
+  it('transfers token ownership', async () => {
+    const tokenInstance = await Token.deployed()
+    const success = await tokenInstance.transfer.call(accounts[1], 250000, { from: accounts[0] })
+    assert.equal(success, true, 'it returns true')
+    const receipt = await tokenInstance.transfer(accounts[1], 250000, { from: accounts[0] })
+    assert.equal(receipt.logs.length, 1, 'triggers one event');
+    assert.equal(receipt.logs[0].event, 'Transfer', 'should be the Transfer event');
+    assert.equal(receipt.logs[0].args._from, accounts[0], 'logs the account the tokens are transfered from');
+    assert.equal(receipt.logs[0].args._to, accounts[1], 'logs the account the tokens are transfered to');
+    assert.equal(receipt.logs[0].args._value, 250000, 'logs the transfer amount');
+    const balanceReceiver = await tokenInstance.balanceOf(accounts[1])
+    assert.equal(balanceReceiver.toNumber(), 250000, 'adds the amount to the receiving account')
+    const balanceSender = await tokenInstance.balanceOf(accounts[0])
+    assert.equal(balanceSender.toNumber(), 750000, 'deducs the amount from the sending account')
+    try {
+      await tokenInstance.transfer.call(accounts[1], 999999999999999)
+    } catch (error) {
       assert(error.message.indexOf('revert') >= 0, 'error message must contain revert')
-      return tokenInstance.transfer.call(accounts[1], 250000, { from: accounts[0] })
-    }).then(success => {
-      assert.equal(success, true, 'it returns true')
-      return tokenInstance.transfer(accounts[1], 250000, { from: accounts[0] })
-    }).then(receipt => {
-      assert.equal(receipt.logs.length, 1, 'triggers one event');
-      assert.equal(receipt.logs[0].event, 'Transfer', 'should be the Transfer event');
-      assert.equal(receipt.logs[0].args._from, accounts[0], 'logs the account the tokens are transfered from');
-      assert.equal(receipt.logs[0].args._to, accounts[1], 'logs the account the tokens are transfered to');
-      assert.equal(receipt.logs[0].args._value, 250000, 'logs the transfer amount');
-      return tokenInstance.balanceOf(accounts[1])
-
-    }).then(balance => {
-      assert.equal(balance.toNumber(), 250000, 'adds the amount to the receiving account')
-      return tokenInstance.balanceOf(accounts[0])
-    }).then(balance => {
-      assert.equal(balance.toNumber(), 750000, 'deducs the amount from the sending account')
-    })
+    }
   });
   it('approves tokens for delegated transfers', async () => {
     const tokenInstance = await Token.deployed()
