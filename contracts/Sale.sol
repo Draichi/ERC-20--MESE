5de96053f726aa0edb43ddfb1582c675ead4e7f5
draichi <lucasdraichi@gmail.com>
2020-02-16 17:47:28 -0300

    wip

--- contracts/Sale.sol
+++ contracts/Sale.sol
@@ -11,7 +11,7 @@ contract Sale {
 
     event Sell(address _buyer, uint256 _amount);
 
-    address admin;
+    address payable admin;
     Token public tokenContract;
     uint256 public tokenPrice;
     uint256 public tokensSold;
@@ -22,7 +22,17 @@ contract Sale {
 
     function buyTokens(uint256 _numberOfTokens) public payable {
         require(msg.value == multiply(_numberOfTokens, tokenPrice), 'require that value is equal to tokens');
+        require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
+        require(tokenContract.transfer(msg.sender, _numberOfTokens));
         tokensSold += _numberOfTokens;
         emit Sell(msg.sender, _numberOfTokens);
+    }
+    function endSale() public {
+        require(msg.sender == admin);
+        require(tokenContract.transfer(admin, tokenContract.balanceOf(address(this))));
+
+        // UPDATE: Let's not destroy the contract here
+        // Just transfer the balance to the admin
+        admin.transfer(address(this).balance);
     }
 }
